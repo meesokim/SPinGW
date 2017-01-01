cc606497eb6a3613f03811d9b10bd79a92853b0c
Miso Kim <meeso.kim@gmail.com>
2017-01-02 01:57:12 +0900

    update for serial communication

--- example.c
+++ example.c
@@ -2,16 +2,20 @@
 #include "serialport.h"
 int main(void)
 {
-	HANDLE h = openSerialPort("COM1",B9600,one,off);
-	char sendbuffer[] = "test";
-	char readbuffer[100];
+	HANDLE h = openSerialPort("COM7",B115200,one,off);
+	int i = 0;
+	char saddr[3];
+	unsigned short retval;
 	//write test
-	int bytesWritten = writeToSerialPort(h,sendbuffer,strlen(sendbuffer));
-	printf("%d Bytes were written\n",bytesWritten);
-	//read something
-	int bytesRead = readFromSerialPort(h,readbuffer,99);
-	readbuffer[bytesRead]=0;
-	printf("%d Bytes were read:%s\n",bytesRead,readbuffer);
+	saddr[0] = 'r';
+	for(i = 0; i < 0x80; i++)
+	{
+		saddr[1] = i & 0xff;
+		saddr[2] = (i >> 8) & 0xff;
+		writeToSerialPort(h,saddr,strlen(saddr));
+		readFromSerialPort(h,&retval,2);
+		printf("0x%04x:%02x\n", i, retval&0xff);
+	}
 	closeSerialPort(h);
     return 0;
 }
